83a47094d0c0fe2bc8c3831b89f4ccd98a45e1b6
dehorus <ruizm.aj@gmail.com>
2020-11-30 16:12:27 -0600

    Reforms

--- js/app.js
+++ js/app.js
@@ -7,5 +7,14 @@ menuIcon.addEventListener('click', () => {
 });
 
 
+const elem = document.querySelector('.main-carousel');
+
+const flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: true
+});
+
 
 
